39c4db2b6e7d6a3c4de992cd33c8722df46d4633
Tobias Aagaard <101559233+TobiasAagaard@users.noreply.github.com>
2024-02-27 08:51:55 +0100

    opsætning til navbar

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom"
+import { Navbar } from "../components/Nav/Navbar"
 
 export const MainLayout = () => {
     return (
         <>
-        <h1>nav</h1>
+        <Navbar />
         <Outlet />
         </>
     )
