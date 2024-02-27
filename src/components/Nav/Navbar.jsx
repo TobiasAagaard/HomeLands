856ccbe81a1be1041ed8a4752cab182b08e967ec
Tobias Aagaard <101559233+TobiasAagaard@users.noreply.github.com>
2024-02-27 09:20:56 +0100

    Color vars

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,9 +1,11 @@
+import { NavLink } from "react-router-dom"
 
 export const Navbar = () => {
     return (
         <nav>
             <ul>
-                <li>nav</li>
+                <li><NavLink to="/">Forside</NavLink></li>
+                <li><NavLink to="Login">Login</NavLink></li>
             </ul>
         </nav>
     )
