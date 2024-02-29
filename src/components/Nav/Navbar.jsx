5f71d57e94577ac4f98cfe783aa8e48315bae27a
Tobias Aagaard <101559233+TobiasAagaard@users.noreply.github.com>
2024-02-29 15:58:26 +0100

    Lidt nyt

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,13 +1,14 @@
 import { NavLink } from "react-router-dom"
+import "./Navbar.scss"
 
 export const Navbar = () => {
     return (
-        <nav>
-            <ul>
-                <li><NavLink to="/">Forside</NavLink></li>
-                <li><NavLink>Boliger til salg</NavLink></li>
-                <li><NavLink to="Login">Login</NavLink></li>
-            </ul>
-        </nav>
+            <nav className="navContainer">
+                <ul>
+                    <li><NavLink to="/">Forside</NavLink></li>
+                    <li><NavLink>Boliger til salg</NavLink></li>
+                    <li><NavLink to="Login">Login</NavLink></li>
+                </ul>
+            </nav>
     )
 }
